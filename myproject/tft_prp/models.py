3fc5fe7ba7f390c3ce78a2cc24968ddd69864684
khamy <khamy.le@live.de>
2020-05-30 16:40:21 +0200

    add models level, unit_cost, hits, times_to_roll and migrate them, add a grid in home.html and change title desgin

--- myproject/tft_prp/models.py
+++ myproject/tft_prp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+# when adding new model:
+#   python manage.py makemigration
+#   python manage.py migrate
+
+class Output(models.Model):
+    level = models.IntegerField()
+    unit_cost = models.IntegerField()
+    hits = models.IntegerField()
+    times_to_roll = models.IntegerField()
+
